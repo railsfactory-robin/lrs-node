b3313d65755bb0113e8bd959f51defa7bc4c485d
Robin Thomas <robinthomas@railsfactory.org>
2019-06-18 12:11:47 +0530

    patch fix

--- services/errorService.js
+++ services/errorService.js
@@ -9,22 +9,21 @@ exports.createErrorLogs = async (error) => {
     return await knex('logs').insert(obj).returning(['id'])
   } catch (err) {
     let error_code = await knex('error').where({ error_code: 'CREATE_USER_FAILED' })
-    let sampleJson = {
-      user: "Robin",
-      date: '17/06/2019'
-    }
-    str = error_code[0].error_message;
-    // str = 'Unable to create {user} user please {date} your fields.'
-    const regex = /\{([^}]+)\}/gm;
-    let array_data = str.match(regex)
-    array_data.map((item, index) => {
-      let result = item.substring(1, item.length - 1);
-      str = str.replace(`{${result}}`, sampleJson[result]);
-    })
-    return { error: err, message: str };
+    return { error: err, message: getErrorParser(error_code[0].error_message) };
   }
 }
 
-
-
+function getErrorParser(str) {
+  let sampleJson = {
+    user: "Robin Thomas",
+    date: '17/06/2019'
+  }
+  const regex = /\{([^}]+)\}/gm;
+  let array_data = str.match(regex)
+  array_data.map((item, index) => {
+    let result = item.substring(1, item.length - 1);
+    str = str.replace(`{${result}}`, sampleJson[result]);
+  })
+  return str;
+}
 
--- services/userService.js
+++ services/userService.js
@@ -1,5 +1,4 @@
 const knex = require('../db')
-const _ = require('lodash')
 
 exports.getAllUsers = async () => {
   return await knex('users').select()
@@ -15,11 +14,7 @@ exports.createUser = async (user) => {
       email: user.email,
       created_on: new Date()
     }
-    return await knex('users').insert(data).returning(
-      _.flattenDeep([
-        'user_id', Object.keys(data)
-      ])
-    )
+    return await knex('users').insert(data).returning('*')
   } catch (err) {
     return { error: err, message: "Something went wrong!" };
   }
@@ -30,11 +25,7 @@ exports.updateUser = async (id, user) => {
     return await knex('users')
       .where({ user_id: parseInt(id) })
       .update(user)
-      .returning(
-        _.flattenDeep([
-          'user_id', Object.keys(user)
-        ])
-      )
+      .returning('*')
   } catch (err) {
     return { error: err, message: "Something went wrong!" };
   }
